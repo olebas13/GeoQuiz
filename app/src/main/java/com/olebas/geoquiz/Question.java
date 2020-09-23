e28025a68d489cb50edc5d60c2a200fad4e0157e
olebas13 <olebas13@gmail.com>
2020-09-23 12:12:13 +0300

    tasks 3 finished

--- app/src/main/java/com/olebas/geoquiz/Question.java
+++ app/src/main/java/com/olebas/geoquiz/Question.java
@@ -4,10 +4,12 @@ public class Question {
 
     private int mTextResId;
     private boolean mAnswerTrue;
+    private boolean mIsSolved;
 
-    public Question(int textResId, boolean answerTrue) {
+    public Question(int textResId, boolean answerTrue, boolean isSolved) {
         mTextResId = textResId;
         mAnswerTrue = answerTrue;
+        mIsSolved = isSolved;
     }
 
     public int getTextResId() {
@@ -24,5 +26,13 @@ public class Question {
 
     public void setAnswerTrue(boolean answerTrue) {
         mAnswerTrue = answerTrue;
+    }
+
+    public boolean isSolved() {
+        return mIsSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mIsSolved = solved;
     }
 }
